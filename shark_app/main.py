1678155e3716d5562aa1febc7e8e5812a82aca84
Radu Burtea <raduburtea@Radus-Laptop.local>
2024-11-22 19:29:08 +0200

    fix endpoint

--- shark_app/main.py
+++ shark_app/main.py
@@ -1,3 +1,5 @@
+import pickle
+
 import uvicorn
 from dotenv import dotenv_values
 from fastapi import FastAPI
@@ -14,6 +16,19 @@ def startup_db_client():
     app.mongodb_client = MongoClient("mongodb://0.0.0.0:27017/")
     app.database = app.mongodb_client[config["DB_NAME"]]
     print("Project connected to the MongoDB database!")
+
+
+@app.on_event("startup")
+async def load_model():
+    global shark_probability_model
+    try:
+        # Option 1: Load a traditional ML model (e.g., scikit-learn)
+        with open("src/ml_models/trained_adaboost.pkl", "rb") as file:
+            shark_probability_model = pickle.load(file)
+        print("Model loaded successfully")
+    except Exception as e:
+        print(f"Error loading model: {str(e)}")
+        raise
 
 
 @app.on_event("shutdown")
--- shark_app/src/endpoints/shark_attacks.py
+++ shark_app/src/endpoints/shark_attacks.py
@@ -2,7 +2,7 @@ from typing import List
 
 import src.rules.shark_attacks as shark_attacks
 from fastapi import APIRouter, Request, status
-from src.models.shark_attacks import SharkAttacks, UpdateSharkAttacks, SharkAttackPred
+from src.models.shark_attacks import SharkAttackPred, SharkAttacks, UpdateSharkAttacks
 
 router = APIRouter(
     prefix="/shark_attacks",
@@ -76,10 +76,12 @@ def list_shark_attacks_by_type(request: Request, type: str):
 def delete_shark_attack(request: Request, id: str):
     return shark_attacks.delete_shark_attack(request, id)
 
+
 @router.delete("/")
 def cleanup_invalid_formats(request: Request):
     return shark_attacks.delete_invalid_shark_attacks(request, 5000)
 
-@router.put("/")
+
+@router.put("/", response_description="Predict Probability of Shark Attack")
 def predict_attack_probability(shark_attack: SharkAttackPred):
     return shark_attacks.predict_shark_attack_probability(shark_attack)
