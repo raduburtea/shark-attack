8726eaea735908594e00d2b5d22ec885655e863a
Radu Burtea <raduburtea@Radus-Laptop.local>
2024-11-22 16:11:10 +0200

    update model class

--- shark_app/src/models/shark_attacks.py
+++ shark_app/src/models/shark_attacks.py
@@ -1,14 +1,64 @@
+import json
+import pickle
 import uuid
 from typing import Optional
 
+from bson import ObjectId
 from dotenv import dotenv_values
 from pydantic import BaseModel, Field, validator
 
 config = dotenv_values(".env")
 
 
-class SharkAttacks(BaseModel):
-    id: str = Field(default_factory=lambda: str(uuid.uuid4), alias="_id")
+class SharkAttackPred(BaseModel):
+    month: str
+    country: str
+    activity: str
+    injuries: str
+    type: str
+
+    @validator("country")
+    def country_must_be_in_countries(cls, country):
+        if country.upper() not in json.loads(config["country"]):
+            raise ValueError(f'Country must be in {config["country"]}')
+        return country
+
+    @validator("month")
+    def month_must_be_in_months(cls, month):
+        if month.lower() not in [col.lower() for col in json.loads(config["month"])]:
+            print([col.lower() for col in config["month"]])
+            raise ValueError(f'Month must be in {config["month"]}')
+        return month
+
+    @validator("type")
+    def type_must_be_in_types(cls, type):
+        if type.lower() not in [col.lower() for col in json.loads(config["type"])]:
+            raise ValueError(f'Typpe must be in {config["type"]}')
+        return type
+
+    @validator("injuries")
+    def injury_must_be_in_injurys(cls, injuries):
+        if injuries.lower() not in [
+            col.lower() for col in json.loads(config["injuries"])
+        ]:
+            raise ValueError(f'Injury must be in {config["injuries"]}')
+        return injuries
+
+    class Config:
+        allow_population_by_field_name = True
+        schema_extra = {
+            "example": {
+                "month": "Sep",
+                "country": "SOUTH AFRICA",
+                "activity": "Swimming",
+                "injuries": "Lasceration",
+                "type": "Unprovoked",
+            }
+        }
+
+
+class SharkAttacks(SharkAttackPred):
+    id: str = Field(default_factory=uuid.uuid4, alias="_id")
     month: str
     country: str
     activity: str
@@ -16,12 +66,6 @@ class SharkAttacks(BaseModel):
     type: str
     year: int
     occurence_per_month: float
-
-    @validator("country")
-    def country_must_be_in_countries(cls, country):
-        if country.upper() not in config["countries"]:
-            raise ValueError(f'Genre must be in {config["countries"]}')
-        return country
 
     class Config:
         allow_population_by_field_name = True
